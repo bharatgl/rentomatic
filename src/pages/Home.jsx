57ba6cca62ac7a97a118bb6168917aeb6ba8292b
bharatgl <goyalbharat9977@gmail.com>
2023-07-26 03:27:49 +0530

    feat: initial header-defaultLayout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import DefaultLayout from "../components/DefaultLayout";
 
 const Home = () => {
   return (
-    <>
+    <DefaultLayout>
       <div>Home</div>
-    </>
+    </DefaultLayout>
   );
 };
 
